cc0b0aa79fdc53b01529286baee7bffa752800ae
fizzahwaseem <fizawaseem66@gmail.com>
2022-01-27 22:28:55 +0500

    partly completed user component

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,10 +1,54 @@
 import React, {Component} from "react";
 
 export default class CreateUsers extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    
+        this.state = {
+          username: '',
+        }
+      }
+
+      onChangeUsername(e) {
+        this.setState({
+          username: e.target.value
+        })
+      }
+
+      onSubmit(e) {
+        e.preventDefault();
+    
+        const user = {
+          username: this.state.username,
+        }
+    
+        console.log(user);
+
+        this.setState({
+            username: ""
+        })
+      }
     render() {
         return (
             <div>
-                <p>You are on the Create Users component!</p>
+                <h3>Create New User</h3>
+                <from onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Username: </label>
+                        <input type="text"
+                        required
+                        className="form-control"
+                        value={this.state.username}
+                        onChange={this.onChangeUsername}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input type="submit" value="Create User" className="btn btn-primary" />
+                    </div>
+                </from>
             </div>
         )
     }
